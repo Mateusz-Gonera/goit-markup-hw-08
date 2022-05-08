a98be105a18462f2aa22b8341bcf8bf7393d53f4
Mateusz-Gonera <mateuszg1992@wp.pl>
2022-05-09 00:06:33 +0200

    page-header tablet

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,12 +1,12 @@
 (() => {
   const refs = {
-    openModalBtn: document.querySelector('[data-mobile-menu-open]'),
-    closeModalBtn: document.querySelector('[data-mobile-menu-close]'),
-    modal: document.querySelector('[data-mobile-menu]'),
+    openMenuBtn: document.querySelector('[data-menu-open]'),
+    closeMenuBtn: document.querySelector('[data-menu-close]'),
+    modal: document.querySelector('[data-menu]'),
   };
 
-  refs.openModalBtn.addEventListener('click', toggleModal);
-  refs.closeModalBtn.addEventListener('click', toggleModal);
+  refs.openMenuBtn.addEventListener('click', toggleModal);
+  refs.closeMenuBtn.addEventListener('click', toggleModal);
 
   function toggleModal() {
     refs.modal.classList.toggle('is-hidden');
